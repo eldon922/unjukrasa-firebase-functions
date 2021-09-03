3817c8a0785f06845425ee3d1414f1031c74ae07
Chris Eldon <chris_eldon@yahoo.com>
2021-09-04 00:11:07 +0700

    first deploy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,71 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const db = admin.firestore();
+
+const functionsJakartaRegion = functions.region("asia-southeast2");
+
+export const onUserCreate =
+  functionsJakartaRegion.firestore.document("/users/{id}")
+      .onCreate((snap) => {
+        return snap.ref.update({
+          demonstrations: [],
+          participation: [],
+          upvote: [],
+          downvote: [],
+          share: [],
+          involve: [],
+        });
+      });
+
+export const onDemonstrationCreate =
+  functionsJakartaRegion.firestore.document("/demonstrations/{id}")
+      .onCreate(async (snap, context) => {
+        const demonstration = snap.data();
+
+        db.collection("users").doc(demonstration.initiatorUID).update({
+          demonstrations: admin.firestore.FieldValue
+              .arrayUnion({id: context.params.id, title: demonstration.title}),
+        });
+
+        const userName = (await db.collection("users")
+            .doc(demonstration.initiatorUID).get()).get("name");
+
+        return snap.ref.update({
+          participation: 0,
+          upvote: 0,
+          downvote: 0,
+          share: 0,
+          persons: [
+            {
+              uid: demonstration.initiatorUID,
+              name: userName,
+            },
+          ],
+        });
+      });
+
+export const demonstrationAction =
+  functionsJakartaRegion.https.onRequest(async (request, response) => {
+    const action = request.query.action as string;
+    const userId = request.query.userId as string;
+    const demonstrationId = request.query.demonstrationId as string;
+
+    const userRef = db.collection("users").doc(userId);
+
+    if (!((await userRef.get()).get(action) as Array<string>)
+        .includes(demonstrationId)) {
+      userRef.update({
+        [action]: admin.firestore.FieldValue.arrayUnion(demonstrationId),
+      });
+
+      db.collection("demonstrations").doc(demonstrationId).update({
+        [action]: admin.firestore.FieldValue.increment(1),
+      });
+
+      response.send(true);
+    } else {
+      response.send(false);
+    }
+  });
